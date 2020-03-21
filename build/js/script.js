49911a97cee89f29479fe967caadfe308c9252d2
g1xus <g1xus@yandex.ru>
2020-03-21 13:30:05 +0300

    made up and adaptated header's navbar block

---
+++ build/js/script.js
@@ -0,0 +1,1 @@
+$(".header-toggler").on("click",function(){$(".header-toggler__element_1").toggleClass("header-toggler__element_1-active"),$(".header-toggler__element_2").toggleClass("header-toggler__element_2-active"),$(".header-toggler__element_3").toggleClass("header-toggler__element_3-active"),$(".header-nav").toggleClass("header-nav_active")});
