8799c1e94c7a8644ad809a68232d26336776c5aa
g1xus <g1xus@yandex.ru>
2020-03-21 18:15:04 +0300

    made up and adaptated hero parallax block

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,4 @@ $('.header-toggler').on('click', function(){
     $('.header-toggler__element_2').toggleClass('header-toggler__element_2-active');
     $('.header-toggler__element_3').toggleClass('header-toggler__element_3-active');
     $('.header-nav').toggleClass('header-nav_active');
-
 });
